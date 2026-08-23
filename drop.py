633df4a1a6c066cfcf24eadb6e397be92bd3d6fd
darioalejandre@gmail.com <darioalejandre@gmail.com>
2026-08-24 00:44:30 +0200

    chore(infra): isolate local database lifecycle

--- drop.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import asyncpg
-
-async def run():
-    conn = await asyncpg.connect('postgresql://postgres:postgres@localhost:5432/sam_db')
-    await conn.execute('DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO public;')
-    await conn.close()
-
-asyncio.run(run())
